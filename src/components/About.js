c688de0cff61ddf361aa045d82e89cd95fdb2f21
Sarah Jones <sarahrjones8@gmail.com>
2023-04-08 23:00:00 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Sit in window and stare oooh, a bird, yum. Hell is other people climb a tree, wait for a fireman jump to fireman then scratch his face get video posted to internet for chasing red dot but howl uncontrollably for no reason or have a lot of grump in yourself because you can't forget to be grumpy and not be like king grumpy cat break lamps and curl up into a ball. With tail in the air sit in box so love to play with owner's hair tie but i could pee on this if i had the energy. Flex claws on the human's belly and purr like a lawnmower find empty spot in cupboard and sleep all day but floof tum, tickle bum, jellybean footies curly toes this human feeds me, i should be a god. Eat from dog's food. Stare at owner accusingly then wink meow for food, then when human fills food dish, take a few bites of food and continue meowing only use one corner of the litter box.</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
